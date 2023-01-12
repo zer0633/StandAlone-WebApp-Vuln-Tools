bd63b7b89430fa9dab644396249c6854ece4245c
zer0633 <94766747+zer0633@users.noreply.github.com>
2023-01-11 19:30:22 -0500

    Update SYNScanner.py

--- SYNScanner.py
+++ SYNScanner.py
@@ -1,12 +1,12 @@
 from scapy.all import *
 from threading import Thread
 
-target_ip = input("Please enter an ip: ")
+host = input("Please enter an ip: ")
 threads = 50
 
-def syn_scan(target_ip,port):
+def syn_scan(host,port):
     src_port = RandShort()
-    packet = sr1(IP(dst=target_ip)/TCP(sport=src_port, dport=port, flags="S"),verbose=0)
+    packet = sr1(IP(dst=host)/TCP(sport=src_port, dport=port, flags="S"),verbose=0)
     if packet[TCP].flags == 18:
         print(f"Port {port} is open")
 
@@ -15,7 +15,7 @@ ports = range(1, 65535)
 
 
 for port in ports:
-	thread = Thread(target=syn_scan, args=(target_ip,port))
+	thread = Thread(target=syn_scan, args=(host,port))
 	thread.daemon = True
 	thread.start()
 	all_threads.append(thread)
